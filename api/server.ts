828db340fcc8d51d9bf8ddba023b9aa76bafc815
SabriBere <95036708+SabriBere@users.noreply.github.com>
2026-03-26 12:07:51 -0300

    refactor and setting API/Socket

--- api/server.ts
+++ api/server.ts
@@ -1,48 +1,28 @@
 import express from "express"
-// import { Request, Response } from "express"
-// import { prisma } from "./config/db"
-import { config } from "dotenv"
 import { WebSocketServer, WebSocket } from "ws";
+import { handlerNewsSocketConnection } from "./sockets/newsSocket";
 import cors from "cors"
 import http from "http";
-config({ path: `./.env.${process.env.NODE_ENV}`})
-//llamado de las los end points, index
+import routes from "./routes/routes"
 
 const server = express()
 
 server.use(express.json());
 server.use(cors());
 
-server.get("/health", (_req, res) => {
-  res.json({ ok: true, message: "Server running" });
-});
-
+//socket
 const httpServer = http.createServer(server);
-
 const wss = new WebSocketServer({ server: httpServer });
 
 wss.on("connection", (socket: WebSocket) => {
-  console.log("Client connected to socket");
-
-  socket.send(
-    JSON.stringify({
-      type: "CONNECTED",
-      message: "Conection success",
-    })
-  );
-
-  socket.on("close", () => {
-    console.log("Client desconected");
-  });
-
-  socket.on("message", (message) => {
-    console.log("Message form client", message.toString());
-  });
+  handlerNewsSocketConnection(socket, wss);
 });
 
+server.use("/", routes);
+
 const PORT = Number(process.env.SOCKET_PORT);
-//socket
-httpServer.listen(PORT, "0.0.0.0", () => {
+
+httpServer.listen(PORT, () => {
   console.log(`Escuchando socket en 0.0.0.0:${PORT}`);
 });
 
