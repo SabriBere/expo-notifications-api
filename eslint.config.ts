3b0179814a0fce0b2868c3591c4dd1c371caa198
Sabrina Demetrio <sdemetrio@globalnewsgroup.com>
2026-03-26 12:36:44 -0300

    esliter + prettier config

---
+++ eslint.config.ts
@@ -0,0 +1,35 @@
+// eslint.config.js
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tseslint.parser,
+      ecmaVersion: 2024,
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      eqeqeq: "error",
+      "no-var": "error",
+      "no-console": "off",
+      "require-await": "warn",
+      "no-unused-vars": "warn",
+      "no-inline-comments": "off",
+      "no-duplicate-imports": "warn",
+      "array-callback-return": "off",
+    },
+  },
+  {
+    name: "prettier",
+    ...prettier,
+  },
+];
